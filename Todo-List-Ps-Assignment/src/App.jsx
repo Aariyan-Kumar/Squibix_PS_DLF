f440180df4eeadadd681052e7478dd36520133c0
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-07-21 00:54:53 +0530

    2nd commit
    Made the application responsive

--- Todo-List-Ps-Assignment/src/App.jsx
+++ Todo-List-Ps-Assignment/src/App.jsx
@@ -55,14 +55,14 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='max-w-5xl bg-purple-200 mx-auto my-16 rounded-md px-5 py-2' style={{ minHeight: '70vh' }}>
-        <h1 className='text-2xl font-bold text-center'>Your Todos List</h1>
+      <div className='w-96 sm:w-8/12 bg-purple-200 mx-auto my-16 rounded-md px-5 py-2' style={{ minHeight: '70vh' }}>
+        <h1 className='text-lg sm:text-xl md:text-2xl font-bold text-center'>Your Todos List</h1>
         <div className="addatodo my-2">
-          <h2 className='text-xl font-medium mx-3 my-4'>Add Your Todo</h2>
-          <div className='flex flex-nowrap justify-evenly'>
+          <h2 className='text-md sm:text-lg md:text-xl font-medium mx-3 my-4'>Add Your Todo</h2>
+          <div className='flex flex-nowrap justify-evenly sm:justify-between '>
             <input
               type="text"
-              className='w-11/12 p-2 rounded-md border border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
+              className='w-8/12 sm:w-9/12 md:w-11/12 mx-0 sm:mx-3 lg:mx-0 p-2 rounded-md border border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'
               onChange={handleChange}
               value={todo}
               required
@@ -70,29 +70,28 @@ function App() {
             <button className='px-4 py-1 rounded-md text-sm font-medium cursor-pointer hover:bg-green-600 hover:text-white transition shadow-md' onClick={() => { handeleAdd() }} style={{ border: '2px solid #4B0082' }} >Save</button>
           </div>
         </div>
-        <h2 className='text-xl font-medium mx-3 my-4'>Your Todos</h2>
+        <h2 className='text-sm sm:text-lg font-medium mx-3 my-4'>Your Todos</h2>
         <div className="todos">
-          {todos.length === 0 && <div className='mx-3 my-2 text-lg text-center'>No Todos to display</div>}
+          {todos.length === 0 && <div className='mx-3 my-2 text-sm sm:text-lg text-center'>No Todos to display</div>}
           {todos.map((item) => {
 
             return (
               <>
-                <div key={item.id} className="todo mx-3 my-2 flex flex-nowrap justify-between">
-                  <div className='flex gap-8'>
+                <div key={item.id} className="todo mx-3 my-2 flex flex-wrap sm:flex-nowrap justify-between items-start">
+                  <div className='flex gap-6 items-start'>
                     <input
                       name={item.id}
                       id=""
                       onChange={handleCheckbox}
                       type="checkbox" value={item.isCompleted}
-                      className='h-5 w-5 bg-green-800 border-gray-300 rounded focus:ring-green-800'
-                      checked = {item.isCompleted ? true : false}
+                      checked={item.isCompleted ? true : false}
                     />
-                    <div className={item.isCompleted ? "line-through" : ""} style={{ display: 'flex', fontSize: '17px' }}>{item.todo}
+                    <div className={`${item.isCompleted ? 'line-through' : ''} flex text-sm sm:text-sm md:text-lg lg:text-xl`}>{item.todo}
                     </div>
                   </div>
-                  <div className="buttons" style={{ minWidth: '200px' }}>
-                    <button className=' mx-2 px-4 py-1 rounded-md text-sm font-medium cursor-pointer hover:bg-yellow-400 hover:text-white transition' onClick={(e) => { handeleEdit(e, item.id) }} style={{ border: '2px solid #4B0082' }}>Edit</button>
-                    <button className='mx-2 px-4 py-1 rounded-md text-sm font-medium cursor-pointer hover:bg-red-600 hover:text-white transition' onClick={(e) => { handeleDelete(e, item.id) }} style={{ border: '2px solid #4B0082' }}>Delete</button>
+                  <div className="buttons flex flex-nowrap" >
+                    <button className=' mx-2 my-3 sm:my-0 px-4 py-1 rounded-md text-sm font-medium cursor-pointer hover:bg-yellow-400 hover:text-white transition' onClick={(e) => { handeleEdit(e, item.id) }} style={{ border: '2px solid #4B0082' }}>Edit</button>
+                    <button className='mx-2 my-3 sm:my-0 px-4 py-1 rounded-md text-sm font-medium cursor-pointer hover:bg-red-600 hover:text-white transition' onClick={(e) => { handeleDelete(e, item.id) }} style={{ border: '2px solid #4B0082' }}>Delete</button>
                   </div>
                 </div>
               </>)
--- Todo-List-Ps-Assignment/src/Components/Navbar.jsx
+++ Todo-List-Ps-Assignment/src/Components/Navbar.jsx
@@ -5,11 +5,11 @@ import img from '../assets/check-list.74bd450f.png';
 function Navbar() {
     return (
         <>
-            <nav className='flex items-center py-7 px-20' style={{ backgroundColor: '#4B0082'}}>
-                <div className='h-16 w-16 mr-5'>
+            <nav className='flex items-center py-3 px-12 sm:py-7 sm:px-20' style={{ backgroundColor: '#4B0082'}}>
+                <div className='h-10 w-10 md:h-16 md:w-16 mr-5'>
                     <img className='w-full h-full' src={img} alt="image logo" />
                 </div>
-                <h1 className='text-3xl text-white m-0 flex-grow font-bold'>My Daily Task</h1>
+                <h1 className='text-2xl md:text-4xl text-center sm:text-left text-white m-0 flex-grow font-bold'>My Daily Task</h1>
             </nav>
         </>
     )
